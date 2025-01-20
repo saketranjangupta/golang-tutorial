6429948089e867c2b6ba629083e84f0d565a998d
sak <saketguptajyg@gmail.com>
2025-01-20 11:49:05 +0530

    add

--- 02variables/main.go
+++ 02variables/main.go
@@ -8,6 +8,7 @@ func main() {
 	var username string = "hitesh"
 	fmt.Println(username)
 	fmt.Printf("Variable is of type: %T \n", username)
+	fmt.Printf("Variable is of type: %T \n", userdata)
 
 	var isLoggedIn bool = false
 	fmt.Println(isLoggedIn)
